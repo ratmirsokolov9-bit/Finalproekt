f2242fcea0e54c887802df622d11332ab788d7d4
ratmirsokolov9-bit <ratmirsokolov9@gmail.com>
2025-12-23 22:00:38 +0300

    Delete 11_Diplom_Proekt directory

--- 11_Diplom_Proekt/test_order_api.py
+++
@@ -1,9 +0,0 @@
-# Соколов Ратмир, 39-я когорта — Финальный проект. Инженер по тестированию плюс
-
-def something_wrong_here(a, b):
-    if a == b:
-        print('Тест пройден')
-    else:
-        print('Тест провален')
-        
-something_wrong_here("c", 'c')
