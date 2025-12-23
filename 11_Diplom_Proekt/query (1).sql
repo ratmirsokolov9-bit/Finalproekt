201a7714cd9a69d27f396ff2ebf66371e6ac59f8
ratmirsokolov9-bit <ratmirsokolov9@gmail.com>
2025-12-23 22:05:21 +0300

    Delete 11_Diplom_Proekt directory

--- 11_Diplom_Proekt/query (1).sql
+++
@@ -1,6 +0,0 @@
-SELECT c.login, COUNT(o.id) AS orders_in_delivery
-FROM "Couriers" c
-JOIN "Orders" o ON c.id = o."courierId"
-WHERE o."inDelivery" = true
-GROUP BY c.login
-ORDER BY orders_in_delivery DESC;
