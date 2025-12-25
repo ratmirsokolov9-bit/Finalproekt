3645403139ceb993d7477fd08b5d381a5280fbce
ratmirsokolov9-bit <ratmirsokolov9@gmail.com>
2025-12-25 15:11:09 +0300

    Update test_data.py

--- test_data.py
+++ test_data.py
@@ -1,11 +1,10 @@
 ORDER_DATA = {
-    "firstName": "Иван",
-    "lastName": "Иванов",
-    "address": "ул. Тестовая, 10",
+    "firstName": "Тест",
+    "lastName": "Тестов",
+    "address": "ул. Тест, 1",
     "metroStation": 4,
-    "phone": "+79991234567",
-    "rentTime": 3,
+    "phone": "+79990000000",
+    "rentTime": 1,
     "deliveryDate": "2024-12-31",
-    "comment": "тестовый заказ",
+    "comment": "тест",
     "color": ["BLACK"]
-}
